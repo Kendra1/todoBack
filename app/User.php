7c38a8d83e9c338d184a906b474aeb65328b155d
Kendra1 <ksydfl@gmail.com>
2019-07-31 12:18:36 +0200

    Created migrations. Created seeders using factory and filled the database with data.

--- app/User.php
+++ app/User.php
@@ -10,13 +10,14 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 'password', 'first_name', 'last_name', 'company', 'country'
     ];
 
     /**
